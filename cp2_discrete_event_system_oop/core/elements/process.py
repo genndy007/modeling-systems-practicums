f3eb96fd7b38effeb997a622d9dcdad02f76688e
genndy007 <kochevgenazp@gmail.com>
2022-10-15 01:15:02 +0300

    Add Process element

--- cp2_discrete_event_system_oop/core/elements/process.py
+++ cp2_discrete_event_system_oop/core/elements/process.py
@@ -2,6 +2,7 @@ from heapq import heapify, heappop, heappush
 
 from core.elements.base import Element
 from core.statistics.process import ProcessStatistics
+from core.utils.classes import Channel
 from core.utils.constants import INFINITY
 from core.utils.functions import set_class_instance_id
 
@@ -30,4 +31,37 @@ class Process(Element):
         channel = heappop(self.channels)
         if self.queue_current_size != 0:
             self.queue_current_size -= 1
+            channel.next_time = self.get_next_time()
+            heappush(self.channels, channel)
+
+        if self.channels:
+            self.next_time = self.channels[0].next_time
+        else:
+            self.next_time = INFINITY
+
+        super().action_out()
+
+    def action_in(self):
+        self.statistics.in_events_amount += 1
+        if len(self.channels) == self.channels_amount:
+            self.__enqueue()
+        else:
+            self.__add_channel()
+
+    def set_current_time(self, next_time):
+        time_difference = next_time - self.current_time
+        self.statistics.time_to_wait += time_difference * self.queue_current_size
+        self.statistics.time_to_work += time_difference * len(self.channels)
+        super().set_current_time(next_time)
+
+    def __enqueue(self):
+        if self.queue_current_size < self.queue_max_size:
+            self.queue_current_size += 1
+        else:
+            self.statistics.fails_amount += 1
+
+    def __add_channel(self):
+        channel = Channel(self.get_next_time())
+        heappush(self.channels, channel)
+        self.next_time = self.channels[0].next_time
 
