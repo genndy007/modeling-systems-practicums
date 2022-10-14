bc48c5de3d438bc7cb4922197a52803d7670afc7
genndy007 <kochevgenazp@gmail.com>
2022-10-15 01:52:23 +0300

    Add gitignore

--- cp2_discrete_event_system_oop/core/utils/functions.py
+++ cp2_discrete_event_system_oop/core/utils/functions.py
@@ -3,5 +3,5 @@ from itertools import count
 
 def set_class_instance_id(cls):
     id_number = count(1)
-    cls.instance_id = lambda: next(id_number)
+    cls.instance_id = lambda arg: next(id_number)
     return cls
